48ab36ab84848611d5ec4ddb3dae66a39237abdb
harithanagaraj <36696201+harithanagaraj@users.noreply.github.com>
2018-02-28 02:23:06 -0800

    c prgm
    good

---
+++ even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+printf("enter the interger value");
+scanf(%d%d%d\numbers");
+if(numbers%2=0)
+printf("%d is even");
+else
+printf("%d is odd");
+getch()
+}
